02085e9877f9adcdc3b095e798407771af54b92e
Ibrahim Eksi <95856789+ibraeksi@users.noreply.github.com>
2025-10-13 23:44:34 +0200

    modified user locations page

--- marketplace_analysis/pages/user_locations.py
+++ marketplace_analysis/pages/user_locations.py
@@ -1,12 +1,11 @@
 import streamlit as st
 import pandas as pd
 from streamlit_folium import st_folium
-import folium
 from modules.navbar import navbar
 from visuals.order_count_city_map import order_count_city_map
 
 
-def seller_locations():
+def user_locations():
     navbar()
 
     st.set_page_config(
@@ -18,14 +17,21 @@ def seller_locations():
 
     df = cached_df
 
-    st.subheader("Analysis based on Seller Locations")
+    st.subheader("Analysis based on User Locations")
 
-    st_folium(order_count_city_map(df, type='seller'), width=725, returned_objects=[])
+    tab1, tab2 = st.tabs([":eyes: Customers", ":shopping_cart: Sellers"])
+
+    with tab1:
+        st.markdown("""Customer Locations with Number of Orders""")
+        st_folium(order_count_city_map(df, type='customer'), width=725, returned_objects=[])
+    with tab2:
+        st.markdown("""Seller Locations with Number of Orders""")
+        st_folium(order_count_city_map(df, type='seller'), width=725, returned_objects=[])
 
 
 if __name__ == '__main__':
     if "data" in st.session_state:
         cached_df = st.session_state["data"]
-        seller_locations()
+        user_locations()
     else:
         st.switch_page('olistAnalysis.py')
